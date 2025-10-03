3a251cd5503c3af1a33683a2ce190ef6a1c0d7b0
eli-veraarreaza <everaarreaza@gmail.com>
2025-10-03 12:27:38 -0300

    Delete grid directory

--- grid/vis/error.js
+++
@@ -1,9 +0,0 @@
-window.onunhandledrejection =
-  window.onerror = (message, source, lineno, colno, error) => {
-    if (!error && message.reason) {
-      error = message.reason;
-      message = error.message;
-    }
-    let div = document.querySelector('#error');
-    div.textContent = message || error.message;
-  };
